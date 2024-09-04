553d4270632b7073d05669a55794ee8fb85d1e28
S Mahmudul Hasan <mahmudulhasan947@gmail.com>
2024-09-03 23:48:29 -0500

    Containerized all of the microservices

--- Microservice/JobMicroService/src/main/java/com/numan947/JobMicroService/job/feignClients/ReviewClient.java
+++ Microservice/JobMicroService/src/main/java/com/numan947/JobMicroService/job/feignClients/ReviewClient.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import java.util.List;
 
-@FeignClient(name = "ReviewMicroService")
+@FeignClient(name = "ReviewMicroService", url="${review-service.url}")
 public interface ReviewClient {
     @GetMapping("/reviews")
     List<Review> getReviews(@RequestParam Long companyId);
